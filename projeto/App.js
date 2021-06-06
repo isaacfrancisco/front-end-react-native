5455f7557fffef9b94f4c6cf975ae1fc7373121f
Isaac Francisco <isaac-silvajf@hotmail.com>
2021-06-06 12:06:21 -0300

    feat: styling text.
    solves task card #F0005.

--- projeto/App.js
+++ projeto/App.js
@@ -40,14 +40,21 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#333',
   },
   postContainer: {
-    margin: 20,
+    marginHorizontal: 20,
+    marginVertical: 10,
     padding: 20,
     backgroundColor: '#fff',
     borderRadius: 3,
   },
+  postTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 5
+  },
+  postDescription: {
+    color: "#666";
+  }
 });
