98c1359d6b479eaed6bac6ab1948034fff565c20
Isaac Francisco <isaac-silvajf@hotmail.com>
2021-06-08 16:46:22 -0300

    feat: add function to authenticate user.
    solves task card #F0005.

--- api/src/App.js
+++ api/src/App.js
@@ -1,11 +1,39 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+import api from './services/api';
 export default class App extends Component {
+  state = {
+    errorMessage: null,
+  };
+
+  signIn = async () => {
+    try {
+      const response = await api.post('/auth/authenticate', {
+        email: 'isaacfranisco123@gmail.com',
+        password: '0123456789',
+      });
+
+      const {user, token} = response.data;
+      console.log(user, token);
+
+      await AsyncStorage.multiSet([
+        ['@CodeApi:token', token],
+        ['@CodeApi:user', JSON.stringify(user)],
+      ]);
+    } catch (response) {
+      this.setState({
+        errorMessage: response.data.error,
+      });
+    }
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text> hello world</Text>
+        {!!this.state.errorMessage && <Text>{this.state.errorMessage}</Text>}
+        <Button onPress={this.signIn} title="Entrar" />
       </View>
     );
   }
