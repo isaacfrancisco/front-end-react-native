b1aceb708d99c97bcfc9533b83fab6067c82013f
Isaac Francisco <isaac-silvajf@hotmail.com>
2021-06-08 12:36:00 -0300

    feat: add "flex" property.
    solves task card #F0005.

--- flexbox/App.js
+++ flexbox/App.js
@@ -17,10 +17,12 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
+    backgroundColor: '#CCC',
+    flex: 1,
   },
   box: {
     height: 100,
-    width: 100,
+    flex: 1,
     backgroundColor: '#666',
     borderColor: '#999',
     borderWidth: 5,
