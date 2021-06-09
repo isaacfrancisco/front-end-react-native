ac7fa8667c5fa99158f2a2c736006cdfd29f9597
Isaac Francisco <isaac-silvajf@hotmail.com>
2021-06-09 17:10:18 -0300

    feat: add apoloClient.
    solves task card #F0005.

--- cqGraphQl/src/App.js
+++ cqGraphQl/src/App.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
+import {ApolloProvider} from 'react-apollo';
 
+import apolloClient from './services/apollo';
 class App extends Component {
   render() {
     return (
-      <View>
-        <Text>Hello World</Text>
-      </View>
+      <ApolloProvider client={apolloClient}>
+        <View>
+          <Text>Hello World</Text>
+        </View>
+      </ApolloProvider>
     );
   }
 }
