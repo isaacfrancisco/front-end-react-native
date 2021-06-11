2c1621b7da090b2c04c688aaf9abe9cdf6e68c74
Isaac Francisco <isaac-silvajf@hotmail.com>
2021-06-10 22:00:44 -0300

    feat: styling "Product" and "ProductList" component.
    solves task card #F0005.

--- layoutrn/src/components/ProductList/Product/index.js
+++ layoutrn/src/components/ProductList/Product/index.js
@@ -1,14 +1,19 @@
 import React, {Component} from 'react';
 import {Image, View, Text} from 'react-native';
+import {Icon} from 'react-native-vector-icons/Ionicons';
 import styles from './styles';
 
 const Product = ({product: {image, title, description, price}}) => {
   return (
     <View style={styles.container}>
-      <Image source={{uri: image}} style={styles.image} />
-      <Text style={styles.title}>{title}</Text>
-      <Text style={styles.description}>{description}</Text>
-      <Text style={styles.price}>{price}</Text>
+      <View style={styles.imageContainer}>
+        <Image source={{uri: image}} style={styles.image} />
+      </View>
+      <View style={styles.infoContainer}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.description}>{description}</Text>
+        <Text style={styles.price}>{price}</Text>
+      </View>
     </View>
   );
 };
