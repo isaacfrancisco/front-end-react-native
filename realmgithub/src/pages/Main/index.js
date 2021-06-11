bca21f8fd9eda8748f2c47da6b12744e22333c03
Isaac Francisco <isaac-silvajf@hotmail.com>
2021-06-11 11:59:11 -0300

    feat: show repositories on main page.
    solves task card #F0005.

--- realmgithub/src/pages/Main/index.js
+++ realmgithub/src/pages/Main/index.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {Container, Title, Form, Input, Submit, List} from './styles';
+import Repository from '../../components/Repository';
 
 export default function Main() {
   return (
@@ -18,6 +19,21 @@ export default function Main() {
           <Icon name="add" size={22} color="#FFF" />
         </Submit>
       </Form>
+
+      <List
+        keyboardShouldPersistTaps="handled"
+        data={[
+          {
+            id: 1,
+            name: 'fdjshfjk',
+            description: 'fjdshfikjas',
+            stars: 1254,
+            forks: 146,
+          },
+        ]}
+        keyExtractor={item => String(item.id)}
+        renderItem={({item}) => <Repository data={item} />}
+      />
     </Container>
   );
 }
