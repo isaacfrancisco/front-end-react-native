be836c5c5ab410704c046708cc89566be42dd84a
Isaac Francisco <isaac-silvajf@hotmail.com>
2021-06-11 10:41:50 -0300

    feat: add test for Todos component.
    solves task card #F0005.

---
+++ testlive/__tests__/Todos.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Todos from '../src/Todos';
+import configureStore from 'redux-mock-store';
+
+const mockStore = configureStore();
+
+const initialState = {
+  todos: [
+    { id: 0, text: 'Fazer café' },
+    { id: 1, text: 'Acessar o slack' },
+    { id: 2, text: 'Ver RocketLive' },
+  ],
+};
+ 
+const store = mockStore(initialState);
+
+//Definindo testes
+describe('Testing Todos Component', () => {
+  it('should render correctly', () => {
+    const wrapper = shallow( 
+      <Todos  /> ,
+      { context: {store} }
+    ).dive();
+
+    expect(wrapper).toMatchSnapshot();
+    wrapper.setProps({ count: 10 });
+    expect(wrapper).toMatchSnapshot();
+  });
+});
