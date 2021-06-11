01e054660156d5398161de0071f64240c56095ed
Isaac Francisco <isaac-silvajf@hotmail.com>
2021-06-11 11:46:34 -0300

    feat: styling Main component.
    solves task card #F0005.

--- realmgithub/src/index.js
+++ realmgithub/src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
-
+import {StatusBar} from 'react-native';
 import '../src/config/ReactotronConfig';
-
 import Routes from '../src/routes';
 
-const App = () => <Routes />;
+const App = () => (
+  <>
+    <StatusBar
+      backgroundColor="transparent"
+      translucent
+      barStyle="light-content"
+    />
+    <Routes />
+  </>
+);
 
 export default App;
--- realmgithub/src/pages/Main/index.js
+++ realmgithub/src/pages/Main/index.js
@@ -1,6 +1,23 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {Container, Title, Form, Input, Submit, List} from './styles';
 
 export default function Main() {
-  return <View />;
+  return (
+    <Container>
+      <Title>Repositório</Title>
+
+      <Form>
+        <Input
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Procurar repositório..."
+        />
+
+        <Submit onPress={() => {}}>
+          <Icon name="add" size={22} color="#FFF" />
+        </Submit>
+      </Form>
+    </Container>
+  );
 }
