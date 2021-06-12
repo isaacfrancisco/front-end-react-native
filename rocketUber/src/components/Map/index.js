d8f51296fc6ea8cad29d2114720bb7fd996e369b
Isaac Francisco <isaac-silvajf@hotmail.com>
2021-06-12 13:59:35 -0300

    feat: add project rocketUber.
    solves task card #F0005.

---
+++ rocketUber/src/components/Map/index.js
@@ -0,0 +1,47 @@
+import React, {Component, Fragment} from 'react';
+import {View, Image} from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
+
+export default class Map extends Component {
+  state = {
+    region: null,
+  };
+  async componentDidMount() {
+    Geolocation.getCurrentPosition(
+      position => {
+        const {coords} = position;
+        getGeoCoderAddress(coords);
+
+        this.setState({
+          ...state,
+          region: {
+            ...state.region,
+            latitude: coords.latitude,
+            longitude: coords.longitude,
+          },
+        });
+      }, // sucesso
+      () => {}, // erro
+      {
+        timeout: 2000,
+        enableHighAccuracy: true,
+        maximumAge: 1000,
+      },
+    );
+  }
+
+  render() {
+    const {region} = this.state;
+    return (
+      <View style={{flex: 1}}>
+        <MapView
+          style={{flex: 1}}
+          region={region}
+          showsUserLocation
+          loadingEnabled
+        />
+      </View>
+    );
+  }
+}
